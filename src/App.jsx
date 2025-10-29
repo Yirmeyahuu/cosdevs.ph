7444b42a79ad57803989f05ec9c54fc7b981850e
Jeremiah Pantaras <jeremiahpantaras@gmail.com>
2025-10-29 11:52:42 +0800

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/layout/Navbar'
 import Home from './pages/Home'
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <div className="bg-brandBlue-darkest min-h-screen">
       <Navbar />
       <Home />
+      <Analytics />
     </div>
   )
 }
