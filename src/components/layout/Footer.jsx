26f36f6d6e4630dffec0d354c398d2a5dc58aa13
Jeremiah Pantaras <jeremiahpantaras@gmail.com>
2026-05-04 18:27:00 +0800

    fixed:logo issue

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -41,7 +41,7 @@ export default function Footer() {
           {/* Column 1 — Brand */}
           <div>
             <img
-              src="/src/assets/images/COSDEVSLOGOTEXT.webp"
+              src="/COSDEVSLOGOTEXT.webp"
               alt="COS Devs"
               className="h-24 w-auto"
             />
